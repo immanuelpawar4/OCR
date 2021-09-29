afb0fc17a32ad1cf79bc1b415781c48d1cc2a520
immanuelpawar4 <immanuelpawar@gmail.com>
2021-09-29 16:14:42 +0530

    this is my first commit for OCR

---
+++ main.py
@@ -0,0 +1,70 @@
+import datetime
+import io
+
+import pytesseract
+from PIL import Image
+from flask import Flask, request, render_template, redirect, url_for, session
+
+app = Flask(__name__)
+
+# Secret key for sessions encryption
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+
+
+@app.route('/')
+def home():
+    return render_template("index.html", title="Simple OCR")
+
+
+@app.route('/scanner', methods=['GET', 'POST'])
+def scan_file():
+    if request.method == 'POST':
+        start_time = datetime.datetime.now()
+        image_data = request.files['file'].read()
+
+        scanned_text = pytesseract.image_to_string(Image.open(io.BytesIO(image_data)))
+
+        print("Found data:", scanned_text)
+
+        session['data'] = {
+            "text": scanned_text,
+            "time": str((datetime.datetime.now() - start_time).total_seconds())
+        }
+
+        return redirect(url_for('result'))
+
+
+@app.route('/result')
+def result():
+    if "data" in session:
+        print("session=", session)
+        data = session['data']
+        print("data=", data['text'])
+        header = data['text'].split("\n")[0]
+        print(header)
+        value = []
+        a = "pawar immanuel"
+        b = a.split(" ")
+        c = a.split(" ")
+        res = ["pawar", "immanuel"]
+        for i in data['text'].split("\n"):
+            value.append(i)
+        response = {
+            header: value
+        }
+        print(response)
+
+        return render_template(
+            "result.html",
+            title="Result",
+            time=data["time"],
+            text=data["text"],
+            words=len(data["text"].split(" "))
+        )
+    else:
+        return "Wrong request method."
+
+
+if __name__ == '__main__':
+    pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+    app.run(debug=True)
